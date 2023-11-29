f773aa4cefc1941673e2f8f06ea9c333d0ed4c63
Bruno Jacobs <brunojacobs@beaver.local>
2023-11-28 22:34:57 -0500

    Add pnpm; prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
