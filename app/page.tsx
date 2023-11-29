17c42db254807797f0d941f68273bed40d39a04f
Guillermo Zumaran <gzumaranbazan@gmail.com>
2023-11-28 22:25:17 -0500

    change import of svgs

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-
+import bottomLogo from '../public/images/bottomdormant.svg';
+import middleLogo from '../public/images/middledormant.svg';
+import topLogo from '../public/images/topdormant.svg';
 export default function Home() {
   return (
     <MaxWidthWrapper>
@@ -43,22 +45,22 @@ export default function Home() {
         {/* Animated Big Logo */}
         <div className="h-auto w-auto">
           <Image
-            src="/images/bottomdormant.svg"
+            src={bottomLogo}
             alt="primer logolayout"
             width={1044.16}
             height={642.56}
             className="absolute -right-80 top-20"
           />
           <Image
-            src="/images/middledormant.svg"
+            src={middleLogo}
             alt="segundo logolayout"
             width={1044.16}
             height={642.56}
             className="absolute -right-64 top-20"
           />
           <Image
-            src="/images/topdormant.svg"
-            alt="segundo logolayout"
+            src={topLogo}
+            alt="tercer logolayout"
             width={1044.16}
             height={642.56}
             className="absolute -right-60 top-20"
