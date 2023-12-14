2a2f3087bc6e2204facc251b1f39e9cc9917783e
Diego <diegogaroban@hotmail.com>
2023-12-14 18:43:13 -0500

    style: add mockup images; add project layout

--- app/projects/giko/page.tsx
+++ app/projects/giko/page.tsx
@@ -1,10 +1,16 @@
 'use client';
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 function Giko() {
   return (
     <main className="px-4 lg:px-[70px] py-32 lg:py-[100px]">
+      <div className="mb-10">
+        <Link href="/" legacyBehavior>
+          <a className="text-blue-600 hover:underline">Regresar</a>
+        </Link>
+      </div>
       <div className="flex flex-col space-y-8 lg:flex lg:flex-row lg:items-center">
         <div className="font-lufgaBold text-4xl lg:text-6xl lg:w-1/3 lg:space-y-10 space-y-4 relative">
           <h1>Giko [+]</h1>
@@ -16,16 +22,16 @@ function Giko() {
         </div>
         <div className="lg:w-2/3 flex items-center justify-center">
           <Image
-            src="/images/giko5.png"
+            src="/images/mockup-giko.png"
             alt="Giko pic"
             width={800}
             height={600}
             placeholder="blur"
-            blurDataURL="/images/giko5.png"
+            blurDataURL="/images/mockup-giko.png"
           />
         </div>
       </div>
-
+      {/* 
       <div className="mt-10 lg:mt-20 rounded-xl">
         <div>
           <Image
@@ -38,7 +44,7 @@ function Giko() {
             className="rounded-xl"
           />
         </div>
-      </div>
+      </div> */}
     </main>
   );
 }
