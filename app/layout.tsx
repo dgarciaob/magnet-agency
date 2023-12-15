016c65f918b9d1280201d8590696cb72479504da
Diego <diegogaroban@hotmail.com>
2023-12-15 09:57:20 -0500

    link preview for socials

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import { cn } from '@/lib/utils';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: 'Magnet',
@@ -14,8 +15,20 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const fullImageUrl = `https://magnetperu.com/logos/logomagnet.png`;
   return (
     <html lang="en" className="h-full">
+      <Head>
+        <meta property="og:image" content={fullImageUrl} />
+        <meta
+          property="og:title"
+          content={String(metadata.title) || 'Magnet'}
+        />
+        <meta
+          property="og:description"
+          content={metadata.description || 'Agencia de Diseño y Desarrollo Web'}
+        />
+      </Head>
       <body
         className={cn('relative antialiased bg-[#F6F5FF] overflow-x-hidden')}
       >
